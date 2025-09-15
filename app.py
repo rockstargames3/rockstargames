9546eb7361d8acde8ec3e0fec2daee19bfbf1306
Rockstar Games <105167048+rockstargames3@users.noreply.github.com>
2025-09-15 13:06:20 +0200

    app.py

--- app.py
+++ app.py
@@ -5,4 +5,13 @@ app = Flask (Rockstar Games)
 
 # Create database and users table (if not exists)
 def init_db():
-    conn = sqlite3.connect()
+    conn = sqlite3.connect('users.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS users
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username admin
+                password admin123)''')
+    conn.commit()
+    conn.close()
+    
+@app.route('/register', methods=['GET', 'POST'])
